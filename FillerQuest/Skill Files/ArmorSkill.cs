97858177c90a397c79d61df766d217cded114282
Cole <charlesengland455@gmail.com>
2020-10-24 19:43:42 -0700

    Changed project references

--- FillerQuest/Skill Files/ArmorSkill.cs
+++ FillerQuest/Skill Files/ArmorSkill.cs
@@ -1,10 +1,4 @@
-﻿using AscendedRPG;
-using ProtoBuf;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ProtoBuf;
 
 namespace AscendedRPG.Skill_Files
 {
